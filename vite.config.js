cbcf855567897a25ba3740ce2c0e3317556abe92
Jaime Glez <jaime01glez@gmail.com>
2025-02-04 21:10:48 +0100

    main

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',  // Asegura que los assets carguen bien en producción
+  build: {
+    outDir: 'dist',  // Define la carpeta de salida correctamente
+  },
+});
